bf9f669fe2da5411366dd3a3ce6d5c9c51250344
jemkein <jemkein@googlemail.com>
2023-10-23 13:15:20 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const production = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  assetPrefix: production ? '/' : '',
+  assetPrefix: production ? '/fettleGeminos/' : 'fettleGeminos/',
   reactStrictMode: true,
   swcMinify: true,
 }
